fad17e21359b34e43ce30da420f7947982257368
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2026-04-13 17:59:28 -0400

    added test-core as dependency

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
+import entities.Person;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Program started.");
+
+		Person p = new Person();
+		String firstName = "first name";
+		p.setFirstName(firstName);
 
 		System.out.println("Program finished.");
 
